569f02a34f7dde7b2e6f141897c49b20eb6c6736
Donovan Bergin <kaiserbergin@gmail.com>
2018-03-13 00:01:32 -0500

    Prefabs for rub goldberg objects created along with test scenes for each

---
+++ Assets/Scripts/Props/AntiGravityFieldGenerator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AntiGravityFieldGenerator : MonoBehaviour {
+
+    private List<Collider> others;
+
+    private void Start() {
+        others = new List<Collider>();
+    }
+
+    // Use this for initialization
+    private void FixedUpdate() {
+        foreach (Collider other in others) {
+            Rigidbody _rigidbody = other.attachedRigidbody;
+            _rigidbody.useGravity = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        others.Add(other);
+    }
+
+    private void OnTriggerExit(Collider other) {
+        Rigidbody _rigidbody = other.attachedRigidbody;
+        _rigidbody.useGravity = true;
+        others.Remove(other);
+    }
+}
